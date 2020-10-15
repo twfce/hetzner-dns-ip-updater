541877c2d1699ad2313ca0454e070eb17d5cbc9e
twfce <me@2fce.de>
2020-10-15 21:03:21 +0200

    Added timestamp to logs

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 import os
 import json
-from time import sleep
 from datetime import datetime
 from colored import fg, attr
 import paho.mqtt.client as mqtt
@@ -10,24 +9,24 @@ from hdns import hdns
 
 def updateDNSRecords(ip):
     api = hdns(hdnsToken)
-    print ("{color}[*] Requesting all zones{reset}".format(color=fg(3), reset=attr(0)))
+    print ("{color}{timestamp} | [*] Requesting all zones{reset}".format(color=fg(3), timestamp=datetime.now(), reset=attr(0)))
     zones = [zone for zone in api.getAllZones() if zone["name"] in config["zones"]] 
 
     for zone in zones:
-        print ("{color}[{zone}] Requesting all registered A records for zone{reset}".format(color=fg(3), zone=zone["name"], reset=attr(0)))
+        print ("{color}{timestamp} | [{zone}] Requesting all registered A records for zone{reset}".format(color=fg(3), timestamp=datetime.now(), zone=zone["name"], reset=attr(0)))
         registered_records = api.getAllRecords(zone["id"], record_type="A")        
-        print ("{color}[{zone}] Checking configured records{reset}".format(color=fg(3), zone=zone["name"], reset=attr(0)))
+        print ("{color}{timestamp} | [{zone}] Checking configured records{reset}".format(color=fg(3), timestamp=datetime.now(), zone=zone["name"], reset=attr(0)))
         for record in config["zones"][zone["name"]]["records"]:
             registered_record = next((rr for rr in registered_records if rr["name"] == record), None)
             if registered_record:
-                print ("{color}[{record}.{zone}] Already registered. Going to update record{reset}".format(color=fg(3), record=record, zone=zone["name"], reset=attr(0)))
+                print ("{color}{timestamp} | [{record}.{zone}] Already registered. Going to update record{reset}".format(color=fg(3), timestamp=datetime.now(), record=record, zone=zone["name"], reset=attr(0)))
                 api.updateRecord(recordId=registered_record["id"], zoneId=zone["id"], name=registered_record["name"], value=ip)
-                print ("{color}[{record}.{zone}] Updated{reset}".format(color=fg(2), record=record, zone=zone["name"], reset=attr(0)))
+                print ("{color}{timestamp} | [{record}.{zone}] Updated{reset}".format(color=fg(2), timestamp=datetime.now(), record=record, zone=zone["name"], reset=attr(0)))
             else:
-                print ("{color}[{record}.{zone}] Not registered. Going to create new record{reset}".format(color=fg(3), record=record, zone=zone["name"], reset=attr(0)))
+                print ("{color}{timestamp} | [{record}.{zone}] Not registered. Going to create new record{reset}".format(color=fg(3), timestamp=datetime.now(), record=record, zone=zone["name"], reset=attr(0)))
 
                 api.createRecord(zoneId=zone["id"], name=record, value=ip)
-                print ("{color}[{record}.{zone}] Created{reset}".format(color=fg(2), record=record, zone=zone["name"], reset=attr(0)))
+                print ("{color}{timestamp} | [{record}.{zone}] Created{reset}".format(color=fg(2), timestamp=datetime.now(), record=record, zone=zone["name"], reset=attr(0)))
 
 def checkIpChanged(report):
     report = json.loads(report)
@@ -39,16 +38,16 @@ def checkIpChanged(report):
         newIP = report['external_ip']
 
         if lastIP == newIP:
-            print ("{color}[*] IP did not change. It's still {IP}{reset}".format(color=fg(3), IP=newIP, reset=attr(0)))
+            print ("{color}{timestamp} | [*] IP did not change. It's still {IP}{reset}".format(color=fg(3), timestamp=datetime.now(), IP=newIP, reset=attr(0)))
         else:
-            print ("{color}[*] IP changed! From {lastIP} to {newIP}{reset}".format(color=fg(214), lastIP=lastIP, newIP=newIP, reset=attr(0)))
+            print ("{color}{timestamp} | [*] IP changed! From {lastIP} to {newIP}{reset}".format(color=fg(214), timestamp=datetime.now(), lastIP=lastIP, newIP=newIP, reset=attr(0)))
             f = open(".remember", "w")
             f.write(newIP)
             f.close()
-            print ("{color}[+] Updated IP cache{reset}".format(color=fg(2), reset=attr(0)))
+            print ("{color}{timestamp} | [+] Updated IP cache{reset}".format(color=fg(2), timestamp=datetime.now(), reset=attr(0)))
             updateDNSRecords(newIP)
     else:
-        print ("{color}[*] This is the first run. IP is {IP}{reset}".format(color=fg(3), IP=newIP, reset=attr(0)))
+        print ("{color}{timestamp} | [*] This is the first run. IP is {IP}{reset}".format(color=fg(3), timestamp=datetime.now(), IP=newIP, reset=attr(0)))
         updateDNSRecords(newIP)
         f = open(".remember", "w")
         f.write(newIP)
@@ -56,17 +55,17 @@ def checkIpChanged(report):
 
 def onMQTTConnect(client, userdata, flags, rc):
     if rc == 0:
-        print ("{color}[+] Connected to MQTT broker (Return code: {rc}){reset}".format(color=fg(2), rc=rc, reset=attr(0)))
+        print ("{color}{timestamp} | [+] Connected to MQTT broker (Return code: {rc}){reset}".format(color=fg(2), timestamp=datetime.now(), rc=rc, reset=attr(0)))
         client.publish("home/public-ip-changer", "Hello World! I'm waiting for reports.")
     else:
-        print ("{color}[-] Not connected to MQTT broker (Return code: {rc}){reset}".format(color=fg(1), rc=rc, reset=attr(0)))
+        print ("{color}{timestamp} | [-] Not connected to MQTT broker (Return code: {rc}){reset}".format(color=fg(1), timestamp=datetime.now(), rc=rc, reset=attr(0)))
 
 
 def onMQTTPublish(client, userdata, mid):
-    print ("{color}[*] Published to MQTT broker (Message ID: {mid}){reset}".format(color=fg(3), mid=mid, reset=attr(0)))
+    print ("{color}{timestamp} | [*] Published to MQTT broker (Message ID: {mid}){reset}".format(color=fg(3), timestamp=datetime.now(), mid=mid, reset=attr(0)))
 
 def onMQTTMessage(client, userdata, msg):
-    print ("{color}[*] Received message on topic {topic}{reset}".format(color=fg(3), topic=msg.topic , reset=attr(0)))
+    print ("{color}{timestamp} | [*] Received message on topic {topic}{reset}".format(color=fg(3), timestamp=datetime.now(), topic=msg.topic , reset=attr(0)))
     print (msg.payload)
     checkIpChanged(msg.payload)
 
@@ -84,7 +83,7 @@ if __name__ == "__main__":
     try:
         hdnsToken = os.environ["HDNS_TOKEN"]
     except KeyError:
-        print ("{color}[X] You need to specify env variable HDNS_TOKEN!{reset}".format(color=fg(1), reset=attr(0)))
+        print ("{color}{timestamp} | [X] You need to specify env variable HDNS_TOKEN!{reset}".format(color=fg(1), timestamp=datetime.now(), reset=attr(0)))
         exit()
     mqttBroker = os.environ["MQTT_BROKER"]
     mqttUsername = os.environ["MQTT_USERNAME"]
@@ -94,3 +93,4 @@ if __name__ == "__main__":
    
     config = json.loads(open("config.json").read())
     main()
+
